a5a0027f85d705e3b57f01b82802f0735ff2aee6
gshemdoe <georgehmariki@gmail.com>
2023-07-03 21:22:29 +0300

    rtbot updated

--- bot.js
+++ bot.js
@@ -480,3 +480,13 @@ process.on('uncaughtException', (err) => {
             console.log(err.message + ' while sending you')
         })
 })
+
+// server for fly.io & adaptable.io
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+});
+
+server.listen(process.env.PORT || 3000, ()=> console.log('Active on port 3000'))
